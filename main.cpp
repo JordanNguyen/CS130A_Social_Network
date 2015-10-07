7a6dc756af21635745ed2f4f9662d182f258c13d
Jordan Nguyen <jordannguyen@umail.ucsb.edu>
2015-10-07 16:46:11 -0700

    fixed

--- main.cpp
+++ main.cpp
@@ -1,23 +1,11 @@
 #include <cstdlib>
 #include <cstdio>
 #include <iostream>
-using namespace std;
-<<<<<<< HEAD
-#include "NodeT.cpp"
-
-int main(int argc, char *argv[])
-{
-	NodeT<int> *test = new NodeT<int>(5);
-	test->printNodeT();
-	test->setValue(7);
-	test->printNodeT();
-=======
 #include "Node.h"
 
 int main(int argc, char *argv[])
 {
 	Node<string> *test = new Node<string>("test");
 	test->printNode();
->>>>>>> 4b7777928ba6c4c7fd8cbf0fa90ddce67e6880f7
 	return 0;
 }
