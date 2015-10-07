c7fce12934ada80896530f4f556ef5d91222cb95
Jordan <jordannguyen95@gmail.com>
2015-10-07 16:43:34 -0700

    updating

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include <cstdio>
 #include <iostream>
 using namespace std;
-#include "NodeT.h"
 #include "NodeT.cpp"
 
 int main(int argc, char *argv[])
 {
 	NodeT<int> *test = new NodeT<int>(5);
+	test->printNodeT();
+	test->setValue(7);
 	test->printNodeT();
 	return 0;
 }
