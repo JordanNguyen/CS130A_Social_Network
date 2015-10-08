c0ccff85739020aa7847b1ca5356f5367c41c17c
Jordan <jordannguyen95@gmail.com>
2015-10-08 00:00:37 -0700

    remove node function fully functional and test

--- LinkedList.h
+++ LinkedList.h
@@ -53,7 +53,69 @@ template <class T> class LinkedList {
 			}
 		}
 
-		//void remove(int pos);
+		/* this function removes the node
+		specified by input position 'pos.'
+		remove() starts counting the first node
+		as index 0, followed by 1, etc */
+		void remove(int pos)
+		{
+			//empty list
+			if (head == NULL && tail == NULL)
+				return;
+
+			//single entry
+			else if (head == tail)
+			{
+				delete head;
+				count--;
+			}
+
+			//if pos is invalid
+			else if (pos < 0)
+			{	
+				std::cout << "Invalid position: less than 0" << std::endl;
+			}
+
+			else if (pos >= count)
+			{
+				std::cout << "Invalid position: greater than or equal to " << 
+				this->getCount() << std::endl;
+			}
+
+			//removing positon 0
+			else if (pos == 0)
+			{
+				Node<T> *temp = head;
+				head = head->getNext();
+				delete temp;
+				count--;
+				return;
+			}
+
+			//removing last element
+			else if (pos == count - 1)
+			{
+				Node<T> *temp = tail;
+				tail = tail->getPrev();
+				delete temp;
+				count--;
+			}
+
+			else
+			{
+				Node<T> *temp = head;
+				//move temp to node removing
+				for(int i = 0; i < pos; i++)
+					temp = temp->getNext();
+				//link the two nodes surrounding temp
+				temp->getPrev()->setNext(temp->getNext());
+				temp->getNext()->setPrev(temp->getPrev());
+				count--;
+			}
+
+			return;
+		}
+
 		int getCount()	 { return count; }
 
 		Node<T> *getHead()  { return head; }
--- main.cpp
+++ main.cpp
@@ -9,17 +9,33 @@ int main(int argc, char *argv[])
 	//Node<string> *test = new Node<string>("test");
 	//test->printNode();
 
-	LinkedList<int> *test = new LinkedList<int>;
-	test->addTail(100);
-	test->addTail(101);
-	test->addTail(103);
-	test->addTail(104);
-	test->addTail(6969);
+	LinkedList<string> *test = new LinkedList<string>;
+	test->addTail("one");
+	test->addTail("two");
+	test->addTail("three");
+	test->addTail("four");
+	test->addTail("five");
 	test->getHead()->printNode();
+	test->getHead()->getNext()->printNode();
+	test->getHead()->getNext()->getNext()->printNode();
+	test->getHead()->getNext()->getNext()->getNext()->printNode();
 	test->getTail()->printNode();
-	test->getTail()->getPrev()->printNode();
-	test->getTail()->getPrev()->getPrev()->printNode();
-	test->getTail()->getPrev()->getPrev()->getPrev()->printNode();
+
+	cout << "there are " << test->getCount() << " nodes" << endl;
+	cout << "removing node index 2" << endl;
+
+	test->remove(2);
+	test->getHead()->printNode();
+	test->getHead()->getNext()->printNode();
+	test->getHead()->getNext()->getNext()->printNode();
+	test->getTail()->printNode();
+	cout << "there are " << test->getCount() << " nodes" << endl;
+
+	test->remove(-1);
+	test->remove(5);
+	test->remove(0);
+	test->getHead()->printNode();
+	cout << "there are " << test->getCount() << " nodes" << endl;
 
 	return 0;
 }
